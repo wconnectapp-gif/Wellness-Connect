9b6ad0fe392d8c3912a384667c8d22c6987c5b00
Somiearitomwest <xxsydney4@gmail.com>
2025-11-28 12:26:59 +0100

    Update About Us and Contact pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,179 @@
-import React from 'react'
+import React from "react";
+import { FaHourglass, FaLeaf, FaSeedling, FaShield } from "react-icons/fa6";
+import { FaFeather } from "react-icons/fa6";
+import MaryImg from "../assets/Mary.jpeg";
+import Well_connectImg from "../assets/well_connect.png";
 
 const About = () => {
   return (
-    <div className='max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12'>About</div>
-  )
-}
+    <>
+      <div className="bg-[#f7f8f8] w-full h-full ">
+        {/* About Us */}
+        <div className="flex justify-center mx-auto items-center h-screen gap-10 py-10 px-30 bg-[#f0fdfa] text-wrap">
+          <div className="flex flex-col max-w-5xl">
+            <h2 className="font-extrabold text-3xl text-gray-600 text-left">
+              About Wellness Connect
+            </h2>
+            <p className=" text-[#4f9791] max-w-2xl mt-5 text-left">
+              Wellness Connect provides a calm space created with intention. A
+              space where individuals, families, and teams reconnect with
+              balance and well-being. At Wellness Connect, we guide you through
+              gentle, mindful practices that support both emotional and physical
+              health. From private sessions to family practices, kids’ yoga, and
+              corporate wellness, we bring calm into the moments that matter.
+              Wellness here is simple, accessible, and designed to help you feel
+              grounded, present, and connected to yourself.
+            </p>
+          </div>
+          <div>
+            <img
+              src={Well_connectImg}
+              alt="A team"
+              className="rounded-xl shadow-2xl"
+            />
+          </div>
+        </div>
 
-export default About
+        {/* Mission and vision section */}
+        <section>
+          <div className="flex pt-20 justify-between max-w-5xl mx-auto  gap-10 text-wrap p-10">
+            <div className="flex flex-col items-start shadow-xl rounded-xl p-3">
+              <FaLeaf size={30} className="text-[#4f9791] mb-1" />
+
+              <h1 className="text-2xl text-gray-600 font-bold text-center ">
+                Our Mission
+              </h1>
+              <p className="text-[#4f9791] m-2">
+                To guide individuals, families, and workplaces toward a more
+                balanced, mindful lifestyle through simple, accessible, and
+                nurturing wellness experiences.
+              </p>
+            </div>
+            <div className="flex flex-col items-start shadow-xl rounded-xl p-3">
+              <FaLeaf size={30} className="text-[#4f9791] mb-1" />
+              <h1 className="text-2xl text-gray-600 font-bold ">Our Vision</h1>
+              <p className="text-[#4f9791] m-2">
+                To inspire a world where wellness feels natural, intentional,
+                and beautifully woven into everyday life — at home, in schools,
+                and in the workplace.
+              </p>
+            </div>
+          </div>
+
+          {/* Our Story */}
+          <section>
+            <div className="flex flex-col items-center p-10 mt-20 text-center max-w-5xl mx-auto">
+              <FaFeather
+                size={30}
+                className="text-[#4f9791] mb-1 text-center flex justify-center"
+              />
+              <h1 className="text-2xl text-gray-600 font-bold ">Our Story</h1>
+              <p className="text-[#4f9791]">
+                Wellness Connect began with a simple intention: to create a
+                space where anyone could experience calm, movement, and
+                connection without pressure or perfection. What started as a
+                passion for mindful movement has grown into a holistic wellness
+                offering supporting: adults seeking personal well-being families
+                and children learning mindful habits schools integrating
+                movement and emotional wellness corporate teams reducing stress
+                and preventing burnout Our approach is gentle, welcoming, and
+                designed for every body — no intimidation, no expectations, just
+                mindful connection.
+              </p>
+            </div>
+          </section>
+        </section>
+        {/* Meet the Founder*/}
+        <section>
+          <div className="flex justify-around mt-20 py-10 px-30 items-center bg-[#f0fdfa] text-wrap gap-5 w-full ">
+            <div className="max-w-5xl ">
+              <h1 className="text-xl text-gray-600 font-bold">
+                Meet The Founder
+              </h1>
+
+              <p className="text-sm text-[#4f9791]">
+                Mary Gbemisola Faniyi — Founder & Wellness Guide
+              </p>
+
+              <p className="text-gray-600 mt-2 text-left">
+                Mary created Wellness Connect from a deep love for calm spaces,
+                emotional well-being, and intentional movement. <br /> Her
+                sessions are guided with warmth, presence, and the belief that
+                wellness should feel nurturing — not overwhelming. <br /> She
+                brings a grounded, heart-centered approach to every session,
+                making wellness accessible to individuals, families, and
+                corporate teams.
+              </p>
+            </div>
+
+            <div className="shadow-2xl">
+              <img
+                src={MaryImg}
+                alt="Mary Gbemisola"
+                className="h-auto w-100 rounded-xl"
+              />
+            </div>
+          </div>
+        </section>
+        {/* Our Values */}
+        <section>
+          <div className="flex flex-col  p-6 mt-20 text-center max-w-5xl mx-auto">
+            <div className=" flex flex-col justify-center items-center max-w-5xl p-10 space-y-3">
+              <FaSeedling size={30} className="text-[#4f9791]" />
+              <h1 className="text-3xl text-gray-600 font-bold ">Our Values</h1>
+
+              <ul className="text-[#4f9791] mt-2 text-center space-y-3 ">
+                <li>Mindfulness in every moment</li>
+                <li>Warm, supportive guidance</li>
+                <li>Simple, accessible wellness </li>
+                <li>Calm spaces for adults and children</li>
+                <li>Intentional and joyful experiences</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        {/* Why people Choose Us */}
+        <section>
+          <div className="flex flex-col items-center p-10 mt-20 text-center mx-auto w-full bg-[#f0fdfa]">
+            <div className="flex flex-col justify-center items-center max-w-5xl p-10 space-y-3">
+              <FaShield
+                size={30}
+                className="text-[#4f9791] mb-1 text-center flex justify-center"
+              />
+              <h1 className="text-3xl text-gray-600 font-bold ">
+                Why People Choose Us
+              </h1>
+              <p className="text-[#4f9791] mt-2 text-center ">
+                Gentle, personalized sessions for individuals, families, and
+                teams Corporate wellness programs that reduce stress and boost
+                focus Flexible options for home, studio, school, office, and
+                events Wellness experiences for all ages Specialty offerings for
+                birthdays, family bonding, and mindful celebrations
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="flex flex-col items-center p-10 mt-20 text-center mx-auto">
+            <div className="flex flex-col justify-center items-center max-w-5xl p-10 space-y-3">
+              <FaHourglass
+                size={30}
+                className="text-[#4f9791] mb-1 text-center flex justify-center"
+              />
+              <h2 className="text-3xl text-gray-600 font-bold ">
+                Ready To Begin Your Wellness Journey?
+              </h2>
+              <p className="text-[#4f9791] mt-2 text-center">
+                Whether it’s personal, family, or corporate wellness, you
+                deserve moments of peace and balance. <br /> Let’s create them
+                together.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,56 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { MdOutlineEmail } from "react-icons/md";
 import { MdOutlineLocalPhone } from "react-icons/md";
 import { LuClock5 } from "react-icons/lu";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 const Contact = () => {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [number, setNumber] = useState();
+  const form = useRef();
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    fullname: "",
+    email: "",
+    number: "",
+    message: "",
+    subject: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          alert("Your message has been sent Successfully!");
+          setFormData({
+            fullname: "",
+            email: "",
+            number: "",
+            subject: "",
+            message: "",
+          });
+        },
+        (error) => {
+          alert("FAILED...", error.text);
+        }
+      )
+      .finally(() => setLoading(false));
+  };
 
   return (
     <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 bg-[#f7f8f8]">
@@ -27,7 +71,7 @@ const Contact = () => {
             <MdOutlineEmail className="text-3xl text-[#1ae6c3]" />
 
             <h2 className="font-semibold text-grey-900 mt-4">Email Us</h2>
-            <p className="text-sm text-[#57616f]">hello@wellnessconnect.com</p>
+            <p className="text-sm text-[#57616f]">wconnectapp@gmail.com</p>
           </div>
           <div className="flex flex-col bg-white w-full text-base h-30 rounded-xl shadow-lg border border-gray-200 items-start p-3 leading-relaxed">
             <MdOutlineLocalPhone className="text-2xl text-[#1ae6c3]" />
@@ -35,7 +79,7 @@ const Contact = () => {
               Call or WhatsApp
             </h6>
             <p className="text-sm text-[#57616f] leading-normal">
-              +233 55 123 4567
+              +234-706-871-5690
             </p>
           </div>
           <div className="flex flex-col bg-white w-full text-base h-30 rounded-xl shadow-lg border border-gray-200 items-start p-3 leading-relaxed ">
@@ -53,83 +97,122 @@ const Contact = () => {
             Contact Form
           </h2>
         </div>
-        {/* Contact Input */}
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-2 p-5">
-          <fieldset>
-            <legend className="text-sm font-semibold text-[#1f2937]">
-              Full Name
-            </legend>
-            <input
-              type="text"
-              placeholder="Enter Your Name"
-              className="form-input rounded-lg border border-gray-300 bg-white focus:outline-none p-3 w-full focus:border-gray-500 focus:ring-0 "
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </fieldset>
-          <fieldset>
-            <legend className="text-sm font-semibold text-[#1f2937]">
-              Email
-            </legend>
-            <input
-              type="email"
-              placeholder="Enter Your Email Address"
-              className="form-input rounded-lg border border-gray-300 bg-white focus:outline-none p-3 w-full focus:border-gray-500"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </fieldset>
-          <fieldset>
-            <legend className="text-sm font-semibold text-[#1f2937]">
-              Phone Number
-            </legend>
-            <input
-              type="tel"
-              placeholder="Enter Your Phone Number"
-              className="form-input rounded-lg border border-gray-300 bg-white focus:outline-none p-3 w-full focus:border-gray-500"
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
-            />
-          </fieldset>
-          <div className="flex flex-col">
-            <label
-              className="text-sm font-semibold text-[#1f2937]"
-              htmlFor="subject"
-            >
-              Subject
-            </label>
-            <select
-              id="subject"
-              className="border border-gray-300 rounded-lg p-3 focus:outline-none w-full bg-white focus:border-gray-500"
-              defaultValue=""
-            >
-              <option value="" disabled>
+        {/* Contact Form */}
+        <form ref={form} onSubmit={handleSubmit}>
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-2 p-5">
+            <fieldset>
+              <legend className="text-sm font-semibold text-[#1f2937]">
+                Full Name
+              </legend>
+              <input
+                name="fullname"
+                type="text"
+                placeholder="Enter Your Name"
+                className="form-input rounded-lg border border-gray-300 bg-white focus:outline-none p-3 w-full focus:border-gray-500 focus:ring-0 "
+                value={formData.fullname}
+                onChange={handleChange}
+              />
+            </fieldset>
+            <fieldset>
+              <legend className="text-sm font-semibold text-[#1f2937]">
+                Email
+              </legend>
+              <input
+                name="email"
+                type="email"
+                placeholder="Enter Your Email Address"
+                className="form-input rounded-lg border border-gray-300 bg-white focus:outline-none p-3 w-full focus:border-gray-500"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </fieldset>
+            <fieldset>
+              <legend className="text-sm font-semibold text-[#1f2937]">
+                Phone Number
+              </legend>
+              <input
+                name="number"
+                type="tel"
+                placeholder="Enter Your Phone Number"
+                className="form-input rounded-lg border border-gray-300 bg-white focus:outline-none p-3 w-full focus:border-gray-500"
+                value={formData.number}
+                onChange={handleChange}
+              />
+            </fieldset>
+            <div className="flex flex-col">
+              <label
+                className="text-sm font-semibold text-[#1f2937]"
+                htmlFor="subject"
+              >
                 Subject
-              </option>
-              <option value="general">Booking Inquiry</option>
-              <option value="support">Reschedule</option>
-              <option value="feedback">Feedback</option>
-              <option value="feedback">Other</option>
-            </select>
+              </label>
+              <select
+                name="subject"
+                id="subject"
+                className="border border-gray-300 rounded-lg p-3 focus:outline-none w-full bg-white focus:border-gray-500"
+                defaultValue=""
+                value={formData.subject}
+                onChange={handleChange}
+              >
+                <option value="" disabled>
+                  Subject
+                </option>
+                <option value="general">Booking Inquiry</option>
+                <option value="support">Reschedule</option>
+                <option value="feedback">Feedback</option>
+                <option value="feedback">Other</option>
+              </select>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col mt-6 p-4">
-          <label
-            className="text-sm leading-normal font-semibold"
-            htmlFor="message"
+          <div className="flex flex-col mt-6 p-4">
+            <label
+              className="text-sm leading-normal font-semibold"
+              htmlFor="message"
+            >
+              Message
+            </label>
+            <textarea
+              className="bg-white h-40 p-2 rounded-lg border border-gray-300 focus:outline-none focus:border-gray-400"
+              name="message"
+              id="message"
+              value={formData.message}
+              onChange={handleChange}
+              placeholder="Type your message here..."
+            ></textarea>
+          </div>
+          <button
+            className="mt-3 bg-[#1ae6c3] p-2 rounded cursor-pointer text-sm font-semibold hover:bg-[#82fce8] transition-all  mx-4
+          "
+            type="submit"
+            disabled={loading}
           >
-            Message
-          </label>
-          <textarea
-            className="bg-white h-40 p-2 rounded-lg border border-gray-300 focus:outline-none focus:border-gray-400"
-            name=""
-            id="message"
-            placeholder="Type your message here..."
-          ></textarea>
-        </div>
-        <button className="mt-3 bg-[#1ae6c3] p-2 rounded cursor-pointer text-sm font-semibold hover:bg-[#82fce8] transition-all  mx-4">
-          Send Message
-        </button>
+            {loading ? (
+              <svg
+                className="animate-spin h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                ></path>
+              </svg>
+            ) : (
+              "Send Message"
+            )}
+          </button>
+        </form>
+
         <div className="bg-white rounded-lg border border-gray-200 p-4 flex justify-between mt-20 py-10 items-center mx-4">
           <div className="flex flex-col">
             <h2 className="text-[#111827] font-bold">
